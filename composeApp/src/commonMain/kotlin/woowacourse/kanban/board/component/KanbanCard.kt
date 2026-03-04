b8fe82c1c1e643d2ab58942760e2add6b6dd9dfd
hanminjae <hanminjae131@gmail.com>
2026-03-05 02:01:49 +0900

    feat: KanbanCard의 title이 비어 있지 않도록 검증 로직을 추가한다.

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/KanbanCard.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/KanbanCard.kt
@@ -37,6 +37,8 @@ fun KanbanCard(
     content: String? = null,
     crewImage: DrawableResource? = null,
 ) {
+    require(title.isNotBlank()) { "KanbanCard의 title은 비어 있을 수 없습니다." }
+
     Column(
         modifier = modifier
             .background(Color.White)
