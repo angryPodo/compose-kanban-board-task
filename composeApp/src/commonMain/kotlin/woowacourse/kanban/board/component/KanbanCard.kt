c6c85d298e29f23c0574487d18d67fe8bac3b6f7
hanminjae <hanminjae131@gmail.com>
2026-03-04 15:33:05 +0900

    feat: 칸반 카드 프레임을 구현한다.

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/KanbanCard.kt
@@ -0,0 +1,37 @@
+package woowacourse.kanban.board.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.resources.DrawableResource
+
+
+@Composable
+fun KanBanCard(
+    title: String,
+    tagList: List<String>,
+    crewName: String,
+    modifier: Modifier = Modifier,
+    content: String? = null,
+    crewImage: DrawableResource? = null,
+) {
+    Column(
+        modifier = modifier
+            .background(Color.White)
+            .clip(RoundedCornerShape(10.dp))
+            .border(Dp.Hairline, Color.Gray, RoundedCornerShape(10.dp))
+            .padding(17.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+
+    }
+}
