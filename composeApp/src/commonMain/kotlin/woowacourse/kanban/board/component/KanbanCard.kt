e0d999ceb119b9efa50af74354d9cc22f29afec6
hanminjae <hanminjae131@gmail.com>
2026-03-04 15:33:40 +0900

    feat: 카드 제목을 구현한다.

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/KanbanCard.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/KanbanCard.kt
@@ -6,12 +6,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.jetbrains.compose.resources.DrawableResource
 
 
@@ -32,6 +36,12 @@ fun KanBanCard(
             .padding(17.dp),
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-
+        Text(
+            text = title,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
     }
 }
