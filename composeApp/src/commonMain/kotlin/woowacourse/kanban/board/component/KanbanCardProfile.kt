dbafb845c08eea37fd274479f091275501bdb32e
hanminjae <hanminjae131@gmail.com>
2026-03-05 02:01:00 +0900

    refactor: KanbanCardProfile 컴포넌트에 원형 클리핑을 적용한다.

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/KanbanCardProfile.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/KanbanCardProfile.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Icon
@@ -12,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,7 +27,7 @@ fun KanbanCardProfile(
     modifier: Modifier = Modifier,
     crewImage: DrawableResource? = null,
 ) {
-    val imageModifier = Modifier.size(24.dp)
+    val imageModifier = Modifier.size(24.dp).clip(CircleShape)
 
     Row(
         modifier = modifier.padding(end = 20.dp, bottom = 10.dp),
